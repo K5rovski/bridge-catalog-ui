7cc891adced12dc6a86485d4eaf9c655004a5699
k5rovski <k5rovski@gmail.com>
2021-04-08 02:49:35 +0200

    put bridge-process as separate backend module

--- bridge-proccess/static/js/index.js
+++ bridge-proccess/static/js/index.js
@@ -1,5 +1,23 @@
 // From Chris Smith's "All Form Elements" pen: https://codepen.io/chris22smith/pen/pymBWL
+import Awesomplete from "./awesomplete.min"
+
 const SEPARATOR = "|||";
+
+
+new Awesomplete('input[data-multiple]', {
+	filter: function(text, input) {
+		return Awesomplete.FILTER_CONTAINS(text, input.match(/[^,]*$/)[0]);
+	},
+
+	item: function(text, input) {
+		return Awesomplete.ITEM(text, input.match(/[^,]*$/)[0]);
+	},
+
+	replace: function(text) {
+		var before = this.input.value.match(/^.+,\s*|/)[0];
+		this.input.value = before + text + ", ";
+	}
+});
 
 function deleteParent(element){
 // Loop through all list items, and hide those who don't match the search query
