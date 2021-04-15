f70deba7e26d9dd168818cab21c474a949bd47bd
k5rovski <k5rovski@gmail.com>
2021-04-15 15:37:06 +0200

    commented unfinished feature request

--- bridge-ui/src/App/components/nav.jsx
+++ bridge-ui/src/App/components/nav.jsx
@@ -10,7 +10,7 @@ function Navbar(){
 		    <div>
 		        <ul>
 		            <li><NavLink to = '/home'>Home </NavLink></li>
-		            <li><NavLink to = '/feature-request'>Feature Request</NavLink></li>
+		            {/*<li><NavLink to = '/feature-request'>Feature Request</NavLink></li>*/}
 		            <li><NavLink to = '/create-role'>Create Role</NavLink></li>
 		            <li><NavLink to = '/create-permission'>Create Permission</NavLink></li>
 		            <li><NavLink to = '/create-setting'>Create Setting</NavLink></li>
