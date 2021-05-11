87cbb165bd790b1992a34f03de2e3ecf2e47ae8e
Kristijan Petrovski <petrovski.kristijan@outlook.com>
2021-05-11 08:07:50 +0200

    setup all packages

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import find_packages
 
 setup(name='bridgescripts',
       version='1.0',
-      packages=find_packages('bridge_scripts'),
+      packages=find_packages('.'),
       )
