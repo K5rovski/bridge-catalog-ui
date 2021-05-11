443ca73e6c8bc7fe8da9716260d3dcd061b5654d
Kristijan Petrovski <petrovski.kristijan@outlook.com>
2021-05-11 09:36:55 +0200

    made gspread saving

--- bridge_scripts/save_dataframe.py
+++ bridge_scripts/save_dataframe.py
@@ -1,3 +1,4 @@
+import string
 import pandas as pd
 
 
@@ -20,7 +21,19 @@ Status
 Feature ID'''.splitlines()
 
 
+def save_gspread(worksheet, pull_csv):
+	sheet_update = [{
+	'range': 'A1:{}1'.format(string.ascii_uppercase[pull_csv.shape[1]-1]),
+	'values': [list(pull_csv.columns)],
+	}]
+	for letter,(ind,col) in zip(string.ascii_uppercase, pull_csv.iteritems()):
+		sheet_update.append({
+			 'range': '{0}2:{0}{1}'.format(letter, pull_csv.shape[0]+1),
+			'values': [["" if pd.isna(i) else i] for i in (col.values)],
+	})
+	worksheet.batch_update(sheet_update)
 
+	
 def save_pull_request_csv(pull_requests, 
 	csv_name='pull_request_GH.csv'):
 	pd_data = [vv for kk,vv in pull_requests.items()]
