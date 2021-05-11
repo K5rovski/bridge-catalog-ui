9e8eb99e13d588d9ac195eda1aa9e48728cad760
Kristijan Petrovski <petrovski.kristijan@outlook.com>
2021-05-11 08:01:46 +0200

    setup files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name='bridge_scripts',
+setup(name='bridgescripts',
       version='1.0',
       packages=find_packages('bridge_scripts'),
       )
